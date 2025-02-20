c74b5c944485bfffb238f8c61d92041869fc14ce
utkarsh764 <utkarshsrivastavalala123@gmail.com>
2025-02-20 18:38:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '-1002244510477') # give your force s
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
-reqst_channel = environ.get('REQST_CHANNEL', '')
+reqst_channel = environ.get('REQST_CHANNEL', 'https://t.me/+IsFfSJGR-VozYTI9')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
